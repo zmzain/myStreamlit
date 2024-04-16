3e0b6712b5f35934ab202e02a0afab7a90b923eb
zmzain <74282751+zmzain@users.noreply.github.com>
2024-04-16 19:13:33 +0300

    Add files via upload

---
+++ iris_model.py
@@ -0,0 +1,17 @@
+
+from sklearn import datasets
+
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+import pickle
+
+iris=datasets.load_iris()
+#print(iris)
+X=iris.data
+y=iris.target
+
+x_train,x_test,y_train,y_test=train_test_split(X,y)
+svc_model=SVC()
+svc_model=svc_model.fit(x_train,y_train)
+
+pickle.dump(svc_model,open('Iris/svc_model.pkl','wb'))
